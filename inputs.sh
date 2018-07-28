fa04cbd4e20dbca556aeb2db74e80b98199ba3a7
ZigaSajovic <ziga.sajovic@gmail.com>
2018-07-28 06:25:12 +0200

    inpuths.sh

---
+++ inputs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#control inputs
+sudo apt install xserver-xorg-input-libinput
+#remap keys when pressed alone vs in combination
+sudo apt-get install xcape 
+#modkeys
+sudo apt-get install xbacklight alsa-utils pulseaudio
+sudo apt-get install feh
