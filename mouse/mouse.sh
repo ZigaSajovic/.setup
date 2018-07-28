bc18b12f69ec40d4749a13c97f42f628f11152e5
ZigaSajovic <ziga.sajovic@gmail.com>
2018-07-28 02:51:52 +0200

    tap-to-click

--- mouse/mouse.sh
+++ mouse/mouse.sh
@@ -2,10 +2,16 @@
 sudo apt install xserver-xorg-input-libinput
 #enable natural scrolling
 #sudo cp naturalScroll.template /usr/share/X11/xorg.conf.d/20-natural-scrolling.conf
-id=$(xinput list-props 13|grep -i natural|head -n1|cut -d"(" -f2|cut -d")" -f1)
-xinput set-prop $id 1
+
+#id of the touchpad
+id=13
+prop_id=$(xinput list-props $id|grep -i natural|head -n1|cut -d"(" -f2|cut -d")" -f1)
+xinput set-prop $id $prop_id 1
 #enable horizontal scroll
-id=$(xinput list-props 13|grep -i hor|head -n1|cut -d"(" -f2|cut -d")" -f1)
-xinput set-prop $id 1
+prop_id=$(xinput list-props $id|grep -i hor|head -n1|cut -d"(" -f2|cut -d")" -f1)
+xinput set-prop $id $prop_id 1
+#enable tap to click
+prop_id=$(xinput list-props $id|grep -i tapping|head -n1|cut -d"(" -f2|cut -d")" -f1)
+xinput set-prop $id $prop_id 1
 #disable middle button
-xinput set-button-map 13 1 0 3
+xinput set-button-map $id 1 0 3
