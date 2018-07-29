45eca13922b2b92573c92420cface2b7c45aa8e7
ZigaSajovic <ziga.sajovic@gmail.com>
2018-07-29 14:52:07 +0200

    i3

---
+++ i3.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#get dependencies
+sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf xutils-dev libtool 
+#i3gaps
+git clone https://www.github.com/Airblader/i3 i3-gaps
+cd i3-gaps
+git checkout gaps && git pull
+autoreconf --force --install
+rm -rf build
+mkdir build
+cd build
+../configure --prefix=/usr --sysconfdir=/etc
+make
+sudo make install
+#i3blocks-gap
+git clone https://www.github.com/Airblader/i3 i3-gaps
+cd i3-gaps
+git checkout gaps && git pull
+autoreconf --force --install
+rm -rf build
+mkdir build
+cd build
+../configure --prefix=/usr --sysconfdir=/etc
+make
+sudo make install
+#other
+sudo apt-get install arandr
+sudo apt-get install feh
+sudo apt-get install lxappearance
+sudo apt-get install compton
+
