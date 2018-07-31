6eeff1ee33cb29abac143e9c3e7a4ca7217c728e
ZigaSajovic <ziga.sajovic@gmail.com>
2018-07-31 14:08:02 +0200

    kodi

---
+++ kodi.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo add-apt-repository ppa:team-xbmc/ppa
+sudo apt-get update
+sudo apt-get install kodi
+
+##if youtube addon does not work, run the following
+#sudo apt-get remove kodi
+#sudo apt-get remove python-openssl python-cryptography python-setuptools python-pkg-resources curl
+#sudo apt-get install kodi
+#sudo apt-get install python-software-properties
+
