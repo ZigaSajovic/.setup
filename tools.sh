ee587ff5c189f911817f4ddac2fcf1dee7a7a9f8
ZigaSajovic <ziga.sajovic@gmail.com>
2018-08-10 06:28:00 +0200

    tools:mupdf

--- tools.sh
+++ tools.sh
@@ -2,7 +2,10 @@
 
 sudo apt-get install autopep8
 sudo apt-get install ranger
-sudo apt-get install okular
+#mupdf
+sudo add-apt-repository ppa:ubuntuhandbook1/apps
+sudo apt-get update
+sudo apt-get install mupdf mupdf-tools 
 #mpv
 sudo add-apt-repository ppa:mc3man/mpv-tests
 sudo apt update && sudo apt install mpv
