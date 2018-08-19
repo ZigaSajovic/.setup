a4260395f46dfac75c19ba12e7b76cedcca6a2a2
ZigaSajovic <ziga.sajovic@gmail.com>
2018-08-20 00:52:57 +0200

    tools: youtube-dl

--- tools.sh
+++ tools.sh
@@ -9,3 +9,6 @@ sudo apt-get install mupdf mupdf-tools
 #mpv
 sudo add-apt-repository ppa:mc3man/mpv-tests
 sudo apt update && sudo apt install mpv
+#youtube-dl
+sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl\
+sudo chmod a+rx /usr/local/bin/youtube-dl
