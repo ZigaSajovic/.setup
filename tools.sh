0ed793d678117396b80528f5b69a308fda68f06a
ZigaSajovic <ziga.sajovic@gmail.com>
2018-08-27 21:06:16 +0200

    qutebrowser

--- tools.sh
+++ tools.sh
@@ -12,3 +12,10 @@ sudo apt update && sudo apt install mpv
 #youtube-dl
 sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl\
 sudo chmod a+rx /usr/local/bin/youtube-dl
+#qutebrowser
+git clone https://github.com/qutebrowser/qutebrowser.git
+cd qutebrowser
+tox -e mkvenv-pypi
+sudo /bin/bash -c  "echo -e '#\!/bin/bash\n~/qutebrowser/.venv/bin/python3 -m qutebrowser \"\$@\"' > /usr/bin/qutebrowser"
+sudo chown root:root /usr/bin/qutebrowser
+sudo chmod 755 /usr/bin/qutebrowser
