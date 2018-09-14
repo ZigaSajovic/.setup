ca7e3b0073f55b4f076bdb18dabdb584e6a736d0
ziga <ZigaSajovic>
2018-09-14 23:25:30 +0200

    tools

--- tools.sh
+++ tools.sh
@@ -5,7 +5,9 @@ sudo apt-get install ranger
 #mupdf
 sudo add-apt-repository ppa:ubuntuhandbook1/apps
 sudo apt-get update
-sudo apt-get install mupdf mupdf-tools 
+sudo apt-get install mupdf mupdf-tools
+#okular
+sudo apt-get install okular
 #mpv
 sudo add-apt-repository ppa:mc3man/mpv-tests
 sudo apt update && sudo apt install mpv
