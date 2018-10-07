631e5dcf6581e066c9bb972a06fe2ecd93246c54
ZigaSajovic <ziga.sajovic@gmail.com>
2018-10-07 23:36:10 +0200

    fixed directories i3

--- i3.sh
+++ i3.sh
@@ -3,6 +3,7 @@
 #get dependencies
 sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf xutils-dev libtool 
 #i3gaps
+cd
 git clone https://www.github.com/Airblader/i3 i3-gaps
 cd i3-gaps
 git checkout gaps && git pull
@@ -15,6 +16,7 @@ make
 sudo make install
 
 #i3blocks-gap
+cd
 git clone https://github.com/Airblader/i3blocks-gaps i3blocks
 cd i3blocks
 make clean debug # or make clean all
